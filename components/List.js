a3a458feef6629339046675bf09d0b0320b204b1
pielc <pierre.le.chanu.pro@gmail.com>
2023-07-29 01:13:00 +0200

    add Map and delete handling

--- components/List.js
+++ components/List.js
@@ -8,11 +8,20 @@ import {
   Spacer,
   Center,
   VStack,
+  IconButton,
 } from "@chakra-ui/react";
 
 import { DeleteIcon } from "@chakra-ui/icons";
+import { useRouter } from "next/navigation";
+
+let list = new Map();
+list.set("111", { unit: "Khal", points: 90, sub_text: "oui" });
+list.set("222", { unit: "Guerriers", points: 135, sub_text: "oui" });
+list.set("333", { unit: "Beserks", points: 135, sub_text: "oui" });
 
 function ListItem(props) {
+  const router = useRouter();
+
   return (
     <Card direction={{ base: "column", sm: "row" }} overflow="hidden" m={2}>
       <CardBody>
@@ -20,7 +29,7 @@ function ListItem(props) {
           <Box>
             <Text>
               {" "}
-              <b>{props.unit_name}</b>{" "}
+              <b>{props.unit}</b>{" "}
             </Text>
             <Text>{props.sub_text}</Text>
           </Box>
@@ -28,8 +37,15 @@ function ListItem(props) {
           <Center>
             <Badge>{props.points}pts</Badge>
           </Center>
-          <Center w="40px">
-            <DeleteIcon />
+          <Center w="40px" marginLeft={2}>
+            <IconButton
+              icon={<DeleteIcon />}
+              variant="ghost"
+              onClick={() => {
+                list.delete(props.data_key);
+                router.refresh();
+              }}
+            />
           </Center>
         </Flex>
       </CardBody>
@@ -38,9 +54,21 @@ function ListItem(props) {
 }
 
 export default function List(props) {
+  const list_items = [];
+  list.forEach((value, key) => {
+    list_items.push(
+      <ListItem
+        key={key}
+        unit={value.unit}
+        points={value.points}
+        sub_text={value.sub_text}
+        data_key={key}
+      />
+    );
+  });
   return (
     <VStack maxWidth={500} align="stretch">
-      <ListItem key={"111"} unit_name={"Khal"} points={90} sub_text={"oui"} />
+      {list_items}
     </VStack>
   );
 }
