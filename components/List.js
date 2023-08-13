a6f3f2d6001746a3225a2ee2dc1c398f2cd17f53
pielc <pierre.le.chanu.pro@gmail.com>
2023-08-14 00:28:42 +0200

    add Add button on Categories

--- components/List.js
+++ components/List.js
@@ -1,23 +1,8 @@
 import React, { useState } from "react";
 
-import { useDisclosure } from "@chakra-ui/react";
-
 import {
-  Box,
-  Text,
-  Badge,
-  Flex,
-  Spacer,
   VStack,
   Button,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  StackDivider,
 } from "@chakra-ui/react";
 
 import ListCategory from "./ListCategory";
@@ -30,22 +15,6 @@ const List = () => {
   const [listOthers, setlistOthers] = useState([
     { unit: "Beserks", points: 135, sub_text: "" },
   ]);
-
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  let addFormFieldsCharacter = (unit, points, sub_text) => {
-    setlistCharacters([
-      ...listCharacters,
-      { unit: unit, points: points, sub_text: sub_text },
-    ]);
-  };
-
-  let addFormFieldsOther = (unit, points, sub_text) => {
-    setlistOthers([
-      ...listOthers,
-      { unit: unit, points: points, sub_text: sub_text },
-    ]);
-  };
 
   let handleShow = () => {
     alert(
@@ -66,58 +35,6 @@ const List = () => {
         Others
       </ListCategory>
       <Button onClick={() => handleShow()}>Show</Button>
-      <Button onClick={onOpen}>Add units</Button>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Add units</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <VStack
-              divider={<StackDivider borderColor="gray.200" />}
-              spacing={1}
-              align="stretch"
-            >
-              <Flex minWidth="max-content" alignItems="center" gap="2">
-                <Box p="2">
-                  <Text as="b">Khal</Text>
-                </Box>
-                <Spacer />
-                <Box>
-                  <Badge>90pts</Badge>
-                </Box>
-                <Button
-                  variant="ghost"
-                  onClick={() => addFormFieldsCharacter("Khal", 90, "")}
-                >
-                  +
-                </Button>
-              </Flex>
-              <Flex minWidth="max-content" alignItems="center" gap="2">
-                <Box p="2">
-                  <Text as="b">Beserk</Text>
-                </Box>
-                <Spacer />
-                <Box>
-                  <Badge>135pts</Badge>
-                </Box>
-                <Button
-                  variant="ghost"
-                  onClick={() => addFormFieldsOther("Beserks", 135, "")}
-                >
-                  +
-                </Button>
-              </Flex>
-            </VStack>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
     </VStack>
   );
 };
--- components/ListCategory.js
+++ components/ListCategory.js
@@ -8,11 +8,28 @@ import {
   Spacer,
   Center,
   IconButton,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  VStack,
+  StackDivider,
+  useDisclosure,
 } from "@chakra-ui/react";
 
 import { DeleteIcon } from "@chakra-ui/icons";
 
 const ListCategory = ({ list, setList, children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  let addFormFields = (unit, points, sub_text) => {
+    setList([...list, { unit: unit, points: points, sub_text: sub_text }]);
+  };
+
   let removeFormFields = (i) => {
     let newlistCharacters = [...list];
     newlistCharacters.splice(i, 1);
@@ -34,6 +51,43 @@ const ListCategory = ({ list, setList, children }) => {
           </Center>
         </Flex>
       </Box>
+      <Button onClick={onOpen}>Add</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack
+              divider={<StackDivider borderColor="gray.200" />}
+              spacing={1}
+              align="stretch"
+            >
+              <Flex minWidth="max-content" alignItems="center" gap="2">
+                <Box p="2">
+                  <Text as="b">Khal</Text>
+                </Box>
+                <Spacer />
+                <Box>
+                  <Badge>90pts</Badge>
+                </Box>
+                <Button
+                  variant="ghost"
+                  onClick={() => addFormFields("Khal", 90, "")}
+                >
+                  +
+                </Button>
+              </Flex>
+            </VStack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
       {list.map((element, index) => (
         <Card
           key={index}
